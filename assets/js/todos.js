68e7b8724be2a98f145063d0a943d4ee25bc6ce6
Gopi Kishan <kishan.gopi14.gk@gmail.com>
2018-03-27 01:06:57 +0530

    enter key correction

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -13,11 +13,13 @@ $("ul").on("click", "span", function(event){
 
 $("input[type='text']").keypress(function(event){
 	if(event.which === 13){
+		if($(this).val()!=""){
 		//grabbing new todo text from input
 		var todoText = $(this).val();
 		$(this).val("");
 		//create a new li and add to ul
 		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+		}
 	}
 });
 
